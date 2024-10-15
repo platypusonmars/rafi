a1845616001f57bd15f79077eee3422669326632
Olga Ivanova <olgaiv.berlin@gmail.com>
2024-10-15 11:24:54 +0200

    refactor: :recycle: Navigation bar refactor; add dark mode support

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { openModal } from '../stores/index';
   import { onMount } from 'svelte';
   import SectionWrapper from './SectionWrapper.svelte';
   let theme: string;
@@ -16,14 +17,57 @@
 </script>
 
 <SectionWrapper id="navigation">
-  <header>
-    <nav>
-      <a href="/">home</a>
-      <a href="/projects">projects</a>
-      <a href="/gallery">gallery</a>
-      <button on:click={toggleTheme} class="dark:text-slate-800">
-        {theme === 'dark' ? '🌞 Light Mode' : '🌜 Dark Mode'}
+  <header class="relative z-20 flex flex-col">
+    <div class="flex flex-row justify-between">
+      <a
+        href="/"
+        class="relative inline-flex items-center px-2 tracking-wider text-white rounded-md bg-slate-800"
+      >
+        <h1 class="relative z-10 font-semibold">
+          Rafi the <span class="text-amber-300">Fluff</span>
+        </h1>
+      </a>
+
+      <button
+        on:click={() => ($openModal = true)}
+        class="grid md:hidden place-items-center"
+      >
+        <i class="fa-solid fa-burger"></i>
       </button>
-    </nav>
+
+      <nav class="items-center justify-between hidden gap-4 md:flex lg:gap-6">
+        <div class="flex items-center justify-center gap-4 px-2">
+          <a href="/" class="duration-200 cursor-pointer hover:text-indigo-400"
+            >home</a
+          >
+          <a
+            href="/projects"
+            class="duration-200 cursor-pointer hover:text-indigo-400"
+            >projects</a
+          >
+          <a
+            href="/gallery"
+            class="duration-200 cursor-pointer hover:text-indigo-400">gallery</a
+          >
+        </div>
+
+        <button
+          on:click={toggleTheme}
+          class={`px-4 py-2 rounded-md transition-colors duration-500 ease-in-out
+            ${theme === 'dark' ? 'bg-slate-800 text-white hover:bg-white hover:text-slate-800' : 'bg-white text-slate-800 hover:bg-slate-800 hover:text-white'}
+            focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600 focus-visible:ring-opacity-50
+            `}
+          aria-label={theme === 'dark'
+            ? 'Switch to Light Mode'
+            : 'Switch to Dark Mode'}
+        >
+          <i
+            class={theme === 'dark'
+              ? 'fa-regular fa-lightbulb'
+              : 'fa-solid fa-lightbulb'}
+          ></i>
+        </button>
+      </nav>
+    </div>
   </header>
 </SectionWrapper>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   darkMode: 'selector', // Enable dark mode based on a CSS class or media query
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      content: {
+        empty: '""', // Empty string for the `before` pseudo-element
+      },
+    },
   },
   plugins: [],
 };
