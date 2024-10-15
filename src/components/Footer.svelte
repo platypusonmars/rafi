52cb1564b0263d7f41f1b284b764a9d32ab0042e
Olga Ivanova <olgaiv.berlin@gmail.com>
2024-10-16 01:36:03 +0200

    style: :lipstick: Style footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,26 +1,30 @@
-<script>
-  import SectionWrapper from './SectionWrapper.svelte';
-</script>
-
 <div
-  class="relative z-20 flex flex-col flex-1 max-w-[1400px] mx-auto w-full p-4 m-2"
+  class="relative z-20 flex flex-col items-center justify-center max-w-[1400px] mx-auto w-full p-4 m-2 text-gray-200"
 >
-  <p>&copy; {new Date().getFullYear()} Rafael Purr Inc. All rights reserved.</p>
-  <div>
+  <p class="mb-4 text-sm text-center text-slate-800 dark:text-amber-400">
+    &copy; {new Date().getFullYear()} Rafael Purr Inc. All rights reserved.
+  </p>
+  <div class="flex gap-6">
     <a
       href="https://github.com/yourusername"
       target="_blank"
-      aria-label="GitHub">GitHub</a
+      aria-label="GitHub"
+      class="text-gray-400 transition-colors duration-300 hover:text-gray-100"
+      >GitHub</a
     >
     <a
       href="https://instagram.com/yourprofile"
       target="_blank"
-      aria-label="Instagram">Instagram</a
+      aria-label="Instagram"
+      class="text-gray-400 transition-colors duration-300 hover:text-gray-100"
+      >Instagram</a
     >
     <a
       href="https://twitter.com/yourusername"
       target="_blank"
-      aria-label="Twitter">Twitter</a
+      aria-label="Twitter"
+      class="text-gray-400 transition-colors duration-300 hover:text-gray-100"
+      >Twitter</a
     >
   </div>
 </div>
